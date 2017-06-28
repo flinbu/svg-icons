8911bfa73f3bedf54dc94325d7fc9dadacd68074
flinbu <flinbu@gmail.com>
2017-06-27 22:40:36 -0500

    Load now verifies if the element is parsed, now you can recall load method to load new icons in doom

--- svg-icons.js
+++ svg-icons.js
@@ -17,17 +17,21 @@ SVGIcons.prototype.load = function() {
     var iconsContainer = document.querySelectorAll('[data-icon]'),
         iconsPath = this.iconsPath;
     iconsContainer.forEach(function(el) {
-        if (typeof SVGRect !== 'undefined') {
-            var icon = el.getAttribute('data-icon'),
-                ajax = new XMLHttpRequest();
-            ajax.open('GET', iconsPath + icon + '.svg', true);
-            ajax.send();
-            ajax.onload = function() {
-                el.innerHTML = ajax.responseText;
-            };
-            el.className = 'svg-icon ' + icon + '-icon';
-        } else {
-            el.setAttribute('data-icon-error', 'SVG is not supported');
+        var loaded = (' ' + el.className + ' ').indexOf(' svg-icon ') > -1;
+        if (!loaded) {
+            if (typeof SVGRect !== 'undefined') {
+                var icon = el.getAttribute('data-icon'),
+                    ajax = new XMLHttpRequest();
+                ajax.open('GET', iconsPath + icon + '.svg', true);
+                ajax.send();
+                ajax.onload = function() {
+                    el.innerHTML = ajax.responseText;
+                };
+                el.className = 'svg-icon ' + icon + '-icon' + ((el.className.length > 0) ? ' ' + el.className : '');
+                el.removeAttribute('data-icon');
+            } else {
+                el.setAttribute('data-icon-error', 'SVG is not supported');
+            }
         }
     });
 };
